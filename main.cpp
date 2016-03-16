4e99c94973b6fa4e79cecfbe6283dd59a8388d3c
Maggie Thomann <mmthomann@gmail.com>
2016-03-16 15:30:30 -0400

    checking to see if comment commit works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "dialog.h"
 #include <QApplication>
 
+// Adding a comment to check if Qt commit works from git windows
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
