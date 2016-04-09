db3299c095d612e14aad4d24f62743d5d50745ef
Daniel Kerrigan <dkerriga@nd.edu>
2016-04-09 18:57:20 -0400

    tabs are working for the most part

--- newBrowser/mainwindow.cpp
+++ newBrowser/mainwindow.cpp
@@ -3,10 +3,14 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow){
     ui->setupUi(this);
-    current = ui->webView;
-    // set homepage to google
-    //ui->webView->load(QUrl("https://www.google.com"));
-    current->load(QUrl("https://www.google.com"));
+    // remove tab that is there by default
+    ui->tabWidget->removeTab(0);
+    // add new tab with web view
+    on_actionNew_Tab_triggered();
+    // set the current web view to be this tab
+    current = (QWebView*) ui->tabWidget->widget(0);
+    // update url edit box text to match url of new tab
+    ui->urlEdit->setText(current->url().toString());
     // when the url is changed, update the url edit box
     connect(current, SIGNAL(urlChanged(const QUrl &)), this, SLOT(updateUrlBox()));
     // when a tab is cicked
@@ -19,22 +23,18 @@ MainWindow::~MainWindow(){
 
 // Go back a page when the back button is clicked
 void MainWindow::on_backButton_clicked(){
-    //ui->webView->back();
     current->back();
 }
 // Go forward a page when the forward button is clicked
 void MainWindow::on_forwardButton_clicked(){
-    //ui->webView->forward();
     current->forward();
 }
 // Load the URL in the URL Edit Box when go is clicked
 void MainWindow::on_goButton_clicked(){
-    //ui->webView->load(QUrl(ui->urlEdit->text()));
     current->load(QUrl(ui->urlEdit->text()));
 }
 // Reload the page when the refresh button is clicked
 void MainWindow::on_refreshButton_clicked(){
-    //ui->webView->reload();
     current->reload();
 }
 // When enter is pressed in the URL edit box, load the page
@@ -43,7 +43,6 @@ void MainWindow::on_urlEdit_returnPressed(){
 }
 // Updat the text in the URL Edit Box to match the true URL
 void MainWindow::updateUrlBox(){
-    //QUrl qurl = ui->webView->url();
     QUrl qurl = current->url();
     QString qurlStr = qurl.toString();
     if(!urls.is_blocked(qurl.host())){ // if the website is not blocked
@@ -51,13 +50,15 @@ void MainWindow::updateUrlBox(){
         if(qurlStr.size() > 0){
             urls.addToHistory(qurlStr);
             ui->urlEdit->setText(qurlStr);
-            //ui->tabWidget->setTabText(ui->tabWidget->currentIndex(), ui->webView->title());
+            ui->tabWidget->setTabText(ui->tabWidget->currentIndex(), qurl.host());
+        } else {
+            qDebug() << "too short";
         }
     } else {  // website is blocked
         // load html for blocked page
         QString html = QString("<html><body><h1>%1 is blocked!</h1><img src='qrc:/Images/emrich.png'></body></html>").arg(qurl.host());
         current->setHtml(html);
-        //ui->tabWidget->setTabText(ui->tabWidget->currentIndex(), "Emrich says no!");
+        ui->tabWidget->setTabText(ui->tabWidget->currentIndex(), "Emrich says no!");
     }
 }
 
@@ -65,7 +66,7 @@ void MainWindow::on_actionNew_Tab_triggered()
 {
     QWebView *newWebView = new QWebView();
     newWebView->setUrl(QUrl("http://www.google.com"));
-    ui->tabWidget->addTab(newWebView, "Test");
+    ui->tabWidget->addTab(newWebView, newWebView->url().host());
 }
 
 void MainWindow::on_actionBack_triggered(){
@@ -75,9 +76,8 @@ void MainWindow::on_actionBack_triggered(){
 void MainWindow::on_actionForward_triggered(){
     on_forwardButton_clicked();
 }
-// change ui->webView to be the webView for the current tab
+// change current to be the web view of the selected tab. update tab title
 void MainWindow::tabSelected(){
-    qDebug() << "tab selected";
     current = (QWebView*) ui->tabWidget->currentWidget();
-    //ui->urlEdit->setText(ui->webView->url().toString());
+    ui->urlEdit->setText(current->url().toString());
 }
