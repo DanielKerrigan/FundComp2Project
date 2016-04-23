bdc83a31129301a3496c1970570a519fef2fd17e
Maggie Thomann <mmthomann@gmail.com>
2016-04-23 10:24:24 -0400

    added the search bar on the ui-still need to fix to resize properly; adding functionality next

---
+++ build-newBrowser-Desktop_Qt_5_5_1_MinGW_32bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,201 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../newBrowser/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[22];
+    char stringdata0[503];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 21), // "on_backButton_clicked"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 24), // "on_forwardButton_clicked"
+QT_MOC_LITERAL(4, 59, 19), // "on_goButton_clicked"
+QT_MOC_LITERAL(5, 79, 24), // "on_refreshButton_clicked"
+QT_MOC_LITERAL(6, 104, 24), // "on_urlEdit_returnPressed"
+QT_MOC_LITERAL(7, 129, 12), // "updateUrlBox"
+QT_MOC_LITERAL(8, 142, 23), // "on_actionBack_triggered"
+QT_MOC_LITERAL(9, 166, 26), // "on_actionForward_triggered"
+QT_MOC_LITERAL(10, 193, 26), // "on_actionNew_Tab_triggered"
+QT_MOC_LITERAL(11, 220, 11), // "tabSelected"
+QT_MOC_LITERAL(12, 232, 9), // "onTimeout"
+QT_MOC_LITERAL(13, 242, 9), // "onClicked"
+QT_MOC_LITERAL(14, 252, 28), // "on_actionNew_Timer_triggered"
+QT_MOC_LITERAL(15, 281, 31), // "on_actionAdd_Bookmark_triggered"
+QT_MOC_LITERAL(16, 313, 29), // "on_actionBlock_site_triggered"
+QT_MOC_LITERAL(17, 343, 31), // "on_actionView_History_triggered"
+QT_MOC_LITERAL(18, 375, 31), // "on_actionView_Blocked_triggered"
+QT_MOC_LITERAL(19, 407, 33), // "on_actionView_Bookmarks_trigg..."
+QT_MOC_LITERAL(20, 441, 31), // "on_actionShow_Timer_2_triggered"
+QT_MOC_LITERAL(21, 473, 29) // "on_actionHide_Timer_triggered"
+
+    },
+    "MainWindow\0on_backButton_clicked\0\0"
+    "on_forwardButton_clicked\0on_goButton_clicked\0"
+    "on_refreshButton_clicked\0"
+    "on_urlEdit_returnPressed\0updateUrlBox\0"
+    "on_actionBack_triggered\0"
+    "on_actionForward_triggered\0"
+    "on_actionNew_Tab_triggered\0tabSelected\0"
+    "onTimeout\0onClicked\0on_actionNew_Timer_triggered\0"
+    "on_actionAdd_Bookmark_triggered\0"
+    "on_actionBlock_site_triggered\0"
+    "on_actionView_History_triggered\0"
+    "on_actionView_Blocked_triggered\0"
+    "on_actionView_Bookmarks_triggered\0"
+    "on_actionShow_Timer_2_triggered\0"
+    "on_actionHide_Timer_triggered"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      20,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  114,    2, 0x08 /* Private */,
+       3,    0,  115,    2, 0x08 /* Private */,
+       4,    0,  116,    2, 0x08 /* Private */,
+       5,    0,  117,    2, 0x08 /* Private */,
+       6,    0,  118,    2, 0x08 /* Private */,
+       7,    0,  119,    2, 0x08 /* Private */,
+       8,    0,  120,    2, 0x08 /* Private */,
+       9,    0,  121,    2, 0x08 /* Private */,
+      10,    0,  122,    2, 0x08 /* Private */,
+      11,    0,  123,    2, 0x08 /* Private */,
+      12,    0,  124,    2, 0x08 /* Private */,
+      13,    0,  125,    2, 0x08 /* Private */,
+      14,    0,  126,    2, 0x08 /* Private */,
+      15,    0,  127,    2, 0x08 /* Private */,
+      16,    0,  128,    2, 0x08 /* Private */,
+      17,    0,  129,    2, 0x08 /* Private */,
+      18,    0,  130,    2, 0x08 /* Private */,
+      19,    0,  131,    2, 0x08 /* Private */,
+      20,    0,  132,    2, 0x08 /* Private */,
+      21,    0,  133,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_backButton_clicked(); break;
+        case 1: _t->on_forwardButton_clicked(); break;
+        case 2: _t->on_goButton_clicked(); break;
+        case 3: _t->on_refreshButton_clicked(); break;
+        case 4: _t->on_urlEdit_returnPressed(); break;
+        case 5: _t->updateUrlBox(); break;
+        case 6: _t->on_actionBack_triggered(); break;
+        case 7: _t->on_actionForward_triggered(); break;
+        case 8: _t->on_actionNew_Tab_triggered(); break;
+        case 9: _t->tabSelected(); break;
+        case 10: _t->onTimeout(); break;
+        case 11: _t->onClicked(); break;
+        case 12: _t->on_actionNew_Timer_triggered(); break;
+        case 13: _t->on_actionAdd_Bookmark_triggered(); break;
+        case 14: _t->on_actionBlock_site_triggered(); break;
+        case 15: _t->on_actionView_History_triggered(); break;
+        case 16: _t->on_actionView_Blocked_triggered(); break;
+        case 17: _t->on_actionView_Bookmarks_triggered(); break;
+        case 18: _t->on_actionShow_Timer_2_triggered(); break;
+        case 19: _t->on_actionHide_Timer_triggered(); break;
+        default: ;
+        }
+    }
+    Q_UNUSED(_a);
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 20)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 20;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 20)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 20;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
